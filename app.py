80e17a14bdd7df837c323927d1e6dec6f895e3a1
Riya Choudhary <156056609+riyachoudhary21@users.noreply.github.com>
2025-07-28 21:08:33 +0530

    removed dynamic spaCy download for streamlit compatibility

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import spacy
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
+    
 import streamlit as st
 import pandas as pd
 import time
